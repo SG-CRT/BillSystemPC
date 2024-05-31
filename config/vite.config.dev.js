a1fa450bf90a6493d886da57a4583f38c612b9ca
SG-CRT <1572225671@qq.com>
2024-05-31 16:09:20 +0800

    eslint配置

--- config/vite.config.dev.js
+++ config/vite.config.dev.js
@@ -1,6 +1,7 @@
-import { mergeConfig } from 'vite';
-import baseConfig from './vite.config.js';
-import VueDevTools from 'vite-plugin-vue-devtools';
+import { mergeConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
+import baseConfig from './vite.config.js'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 
@@ -16,8 +17,12 @@ export default mergeConfig(
     plugins: [
       //调试工具
       VueDevTools(),
-
+      eslint({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+        exclude: ['node_modules'],
+      }),
     ],
   },
   baseConfig,
-);
+)
